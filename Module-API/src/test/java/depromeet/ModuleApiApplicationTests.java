ad03e5f051b910e1b4c3e7f44d883df7e1736be7
imdh <eastlake520@gmail.com>
2023-04-27 22:55:29 +0900

    build: project setup

---
+++ Module-API/src/test/java/depromeet/ModuleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package depromeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
