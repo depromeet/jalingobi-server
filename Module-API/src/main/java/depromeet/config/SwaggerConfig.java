23ed99446998cec25e619586a70d92bf86dd358e
likelasttime <a50070863@gmail.com>
2023-05-06 00:29:46 +0900

    build: Swagger 의존성 수정
    Unable to infer base url문제를 해결하기 위해 수정

--- Module-API/src/main/java/depromeet/config/SwaggerConfig.java
+++ Module-API/src/main/java/depromeet/config/SwaggerConfig.java
@@ -1,44 +1,26 @@
 package depromeet.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.context.annotation.Configuration;
 
-import java.util.HashSet;
-import java.util.Set;
-
+@OpenAPIDefinition(
+        info = @Info(title = "자린고비 API 명세서",
+                description = "Depromeet 13기 1팀 API 명세서",
+                version = "v1"))
+@Configuration
 public class SwaggerConfig {
 
-    private ApiInfo swaggerInfo() {
-        return new ApiInfoBuilder().title("13th-1team API")
-                .description("13th-1team server docs").build();
-    }
-
     @Bean
-    public Docket swaggerApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .consumes(getConsumeContentTypes())
-                .produces(getProduceContentTypes())
-                .apiInfo(swaggerInfo()).select()
-                //.apis(RequestHandlerSelectors.basePackage(""))
-                .paths(PathSelectors.any())
-                .build()
-                .useDefaultResponseMessages(false);
-    }
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/**"};
 
-    private Set<String> getConsumeContentTypes() {
-        Set<String> consumes = new HashSet<>();
-        consumes.add("application/json;charset=UTF-8");
-        consumes.add("application/x-www-form-urlencoded");
-        return consumes;
-    }
-
-    private Set<String> getProduceContentTypes() {
-        Set<String> produces = new HashSet<>();
-        produces.add("application/json;charset=UTF-8");
-        return produces;
+        return GroupedOpenApi.builder()
+                .group("자린고비 API v1")
+                .pathsToMatch(paths)
+                .build();
     }
 }
+
