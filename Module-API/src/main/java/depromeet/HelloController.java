127542a73ba6be699980ee59459cf7ed1622eda8
ParkJungYoon <tbr06057@naver.com>
2023-05-07 00:18:21 +0900

    feat: health check용 endpoint 추가

---
+++ Module-API/src/main/java/depromeet/HelloController.java
@@ -0,0 +1,15 @@
+package depromeet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "hello world";
+    }
+}
