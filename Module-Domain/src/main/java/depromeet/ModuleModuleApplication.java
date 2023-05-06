c2837a72c7a1c0936bd9ded18b03f2f4e8c9acb1
imdh <eastlake520@gmail.com>
2023-05-06 10:15:37 +0900

    chore: package structure 설정

---
+++ Module-Domain/src/main/java/depromeet/ModuleModuleApplication.java
@@ -0,0 +1,6 @@
+package depromeet;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleModuleApplication {}
---
+++ Module-Domain/src/main/java/depromeet/user/domain/User.java
@@ -0,0 +1,19 @@
+package depromeet.user.domain;
+
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+
+//@Entity
+public class User {
+    
+//    @Id
+    private Long id;
+
+
+
+    // 추가
+
+
+    // 검증
+
+}
