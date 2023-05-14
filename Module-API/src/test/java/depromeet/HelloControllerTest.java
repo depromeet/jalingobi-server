a4614942d4b03c351609949383d4a3f45df97a1e
Judy <97580782+ParkJungYoon@users.noreply.github.com>
2023-05-14 12:48:14 +0900

    style: Spotless(코드 포멧팅) 적용 (#6)

--- Module-API/src/test/java/depromeet/HelloControllerTest.java
+++ Module-API/src/test/java/depromeet/HelloControllerTest.java
@@ -1,5 +1,9 @@
 package depromeet;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,23 +12,18 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @WebMvcTest(controllers = {HelloController.class})
 @Import(HelloController.class)
 @DisplayName("[Controller] Health check")
 class HelloControllerTest {
-    @Autowired
-    MockMvc mvc;
+    @Autowired MockMvc mvc;
 
     @Test
     @DisplayName("[GET] Health check test")
     void testHealthCheckTest() throws Exception {
-        mvc.perform(get("/")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
+        mvc.perform(
+                        get("/").contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello world"));
     }
