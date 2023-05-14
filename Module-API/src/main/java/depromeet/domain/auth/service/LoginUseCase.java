b4e103dced1dd0bcb6c3992627052c955eafb52d
ParkJungYoon <tbr06057@naver.com>
2023-05-14 12:39:32 +0900

    style: spotless 컨밴션 적용

--- Module-API/src/main/java/depromeet/domain/auth/service/LoginUseCase.java
+++ Module-API/src/main/java/depromeet/domain/auth/service/LoginUseCase.java
@@ -1,14 +1,6 @@
 package depromeet.domain.auth.service;
 
-/**
- * 비즈니스 검증 로직
- * adapter 사용
- * 매핑 로직 추가
- */
+/** 비즈니스 검증 로직 adapter 사용 매핑 로직 추가 */
 
-//단순 매핑후에 domain쪽으로 넘겨줄지, 아니면 매핑과 로직까지 가져갈지
-public class LoginUseCase {
-
-}
-
-
+// 단순 매핑후에 domain쪽으로 넘겨줄지, 아니면 매핑과 로직까지 가져갈지
+public class LoginUseCase {}
--- Module-Domain/src/main/java/depromeet/user/service/LoginService.java
+++ Module-Domain/src/main/java/depromeet/user/service/LoginService.java
@@ -1,9 +1,7 @@
 package depromeet.user.service;
 
 // 한 개 이상의 도메인 메서드를 묶거나, db 접근이 필요할 경우
-public class LoginService {
-
-}
+public class LoginService {}
 
 // controller -> service -> repository
 
