a4e8f8d55a6a70007b84b1a7cebe751b4ff8b0eb
likelasttime <a50070863@gmail.com>
2023-05-23 15:44:54 +0900

    feat: 예외 처리 추가 (#13)

---
+++ common/src/test/java/depromeet/CommonApplicationTests.java
@@ -0,0 +1,12 @@
+package depromeet;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
