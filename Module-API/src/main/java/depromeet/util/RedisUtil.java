671e632fd5e1df1b0d0cf9560a6598fe4da259d7
likelasttime <a50070863@gmail.com>
2023-05-25 22:22:50 +0900

    feat: util 추가 (#15)

---
+++ Module-API/src/main/java/depromeet/util/RedisUtil.java
@@ -0,0 +1,35 @@
+package depromeet.util;
+
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisUtil {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public String getData(String key) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public void setData(String key, String value) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        valueOperations.set(key, value);
+    }
+
+    public void setDataExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        Duration expiredDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expiredDuration);
+    }
+
+    public void deleteData(String key) {
+        stringRedisTemplate.delete(key);
+    }
+}
