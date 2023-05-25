00b5dc8cc48b8451966ae996d26a1eacdf057229
Judy <97580782+parkjungyoon@users.noreply.github.com>
2023-05-25 13:20:32 +0900

    feature: Common 모듈 생성 (#14)
    * build: common 모듈 build 설정 (#13)
    
    * feat: 예외 처리 추가 (#13)
    
    * feat: 공통 응답 추가 (#13)
    
    ResponseService를 DI해서 사용할 수 있음
    
    * rename: 모듈명 첫 글자를 대문자로 변경 (#13)
    
    * refactor: 모듈 명 변경 (Common -> Module-Common)
    
    * feat(common): annotation 추가
    
    * refactor(common): 폴더 명 변경 (dto -> response)

---
+++ Module-Common/src/main/java/depromeet/response/CommonResponse.java
@@ -0,0 +1,18 @@
+package depromeet.response;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CommonResponse {
+
+    protected Boolean isSuccess;
+
+    protected int code;
+
+    protected String message;
+}
