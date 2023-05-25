f02efbf8fbdb63059e0bd3d4ca64fc99aaea77a0
likelasttime <a50070863@gmail.com>
2023-05-25 22:29:48 +0900

    feat: security filter 추가 (#15)
    OncePerRequestFilter를 상속받아서 각 요청 당 한 번만 처리될 수 있도록 함

---
+++ Module-API/src/main/java/depromeet/security/filter/JwtRequestFilter.java
@@ -0,0 +1,39 @@
+package depromeet.security.filter;
+
+
+import depromeet.exception.CustomExceptionStatus;
+import depromeet.util.JwtUtil;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse httpServletResponse,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = jwtUtil.resolveToken(request);
+        if (token == null) {
+            request.setAttribute(
+                    "exception", CustomExceptionStatus.NOT_AUTHENTICATED_ACCOUNT.getMessage());
+        } else if (jwtUtil.validateToken(token, request)) {
+            Authentication authentication = jwtUtil.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, httpServletResponse);
+    }
+}
