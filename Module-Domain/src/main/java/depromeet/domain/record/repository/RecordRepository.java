2786fdce8939aed63452e6a2454e5bf785a19483
ParkJungYoon <tbr06057@naver.com>
2023-05-30 00:53:20 +0900

    feat: RecordRepository 추가

---
+++ Module-Domain/src/main/java/depromeet/domain/record/repository/RecordRepository.java
@@ -0,0 +1,6 @@
+package depromeet.domain.record.repository;
+
+import depromeet.domain.record.domain.Record;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecordRepository extends JpaRepository<Record, Long> {}
