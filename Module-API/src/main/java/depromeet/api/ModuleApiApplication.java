64af7babf89c5d39add7d3e4feec25c0656a4120
imdh <eastlake520@gmail.com>
2023-05-29 14:28:34 +0900

    bug: Domain 모듈의 @Entity 인지하도록 @EntityScan 설정

--- Module-API/src/main/java/depromeet/api/ModuleApiApplication.java
+++ Module-API/src/main/java/depromeet/api/ModuleApiApplication.java
@@ -5,15 +5,17 @@ import depromeet.common.ModuleCommonApplication;
 import depromeet.domain.ModuleDomainApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @ComponentScan(
         basePackageClasses = {
             ModuleApiApplication.class,
             ModuleCommonApplication.class,
             ModuleDomainApplication.class
         })
+@EntityScan(basePackageClasses = {ModuleDomainApplication.class})
+@SpringBootApplication
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
--- Module-Common/src/main/java/depromeet/common/annotation/Adaptor.java
+++ Module-Common/src/main/java/depromeet/common/annotation/Adaptor.java
@@ -4,12 +4,11 @@ package depromeet.common.annotation;
 import java.lang.annotation.*;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RestController;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@RestController
+@Component
 public @interface Adaptor {
     @AliasFor(annotation = Component.class)
     String value() default "";
