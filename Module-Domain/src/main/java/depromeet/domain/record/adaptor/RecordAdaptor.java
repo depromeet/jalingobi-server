5feeb9d71027da87d91d67802fc2a1ef44a30f2f
ParkJungYoon <tbr06057@naver.com>
2023-05-30 00:56:12 +0900

    feat: RecordAdaptor 추가

---
+++ Module-Domain/src/main/java/depromeet/domain/record/adaptor/RecordAdaptor.java
@@ -0,0 +1,12 @@
+package depromeet.domain.record.adaptor;
+
+import depromeet.annotation.Adaptor;
+import depromeet.domain.record.repository.RecordRepository;
+import lombok.RequiredArgsConstructor;
+
+@Adaptor
+@RequiredArgsConstructor
+public class RecordAdaptor {
+    private final RecordRepository recordRepository;
+
+}
