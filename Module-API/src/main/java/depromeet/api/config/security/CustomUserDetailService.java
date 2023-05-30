4943a8572d62203cdf44ec3307c5d13cedd6c394
ParkJungYoon <tbr06057@naver.com>
2023-05-30 19:40:43 +0900

    refactor: findUser adaptor 에서 예외처리하도록 수정

--- Module-API/src/main/java/depromeet/api/config/security/CustomUserDetailService.java
+++ Module-API/src/main/java/depromeet/api/config/security/CustomUserDetailService.java
@@ -1,11 +1,8 @@
 package depromeet.api.config.security;
 
 
-import depromeet.common.exception.CustomException;
-import depromeet.common.exception.CustomExceptionStatus;
 import depromeet.domain.user.adaptor.UserAdaptor;
 import depromeet.domain.user.domain.User;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,9 +17,7 @@ public class CustomUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String socialId) throws UsernameNotFoundException {
-        Optional<User> account = userAdaptor.findUser(socialId);
-        if (!account.isPresent())
-            throw new CustomException(CustomExceptionStatus.ACCOUNT_NOT_FOUND);
-        return new CustomUserDetails(account.get());
+        User account = userAdaptor.findUser(socialId);
+        return new CustomUserDetails(account);
     }
 }
