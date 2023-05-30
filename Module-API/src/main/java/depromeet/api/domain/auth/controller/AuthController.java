a7f817ac3cd5f5cf8bc4c0c8557877b44650bbe3
likelasttime <a50070863@gmail.com>
2023-05-30 23:43:56 +0900

    feat: refreshToken을 Cookie에서 가져오도록 변경 (#36)
    refreshToken은 Cookie에 보관할 것이라서 수정함

--- Module-API/src/main/java/depromeet/api/domain/auth/controller/AuthController.java
+++ Module-API/src/main/java/depromeet/api/domain/auth/controller/AuthController.java
@@ -4,9 +4,11 @@ package depromeet.api.domain.auth.controller;
 import depromeet.api.domain.auth.dto.request.AuthRequest;
 import depromeet.api.domain.auth.service.AuthService;
 import depromeet.api.domain.auth.usecase.KakaoAuthUseCase;
+import depromeet.api.util.CookieUtil;
 import depromeet.common.response.CommonResponse;
 import depromeet.common.response.ResponseService;
 import depromeet.common.temp.TokenInfo;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +22,9 @@ public class AuthController {
 
     private final ResponseService responseService;
     private final KakaoAuthUseCase kakaoAuthUseCase;
-    private final AuthService authUseCase;
+    private final AuthService authService;
+    private final CookieUtil cookieUtil;
+    private static final String REFRESH_TOKEN = "refreshToken";
 
     @PostMapping("/auth/kakao")
     public CommonResponse auth(@RequestBody AuthRequest reqAuth) {
@@ -31,9 +35,10 @@ public class AuthController {
     @PostMapping("/auth/refresh")
     public CommonResponse refreshToken(
             @RequestHeader("AUTHORIZATION") String token,
-            @RequestHeader("REFRESH-TOKEN") String refreshToken,
+            HttpServletRequest httpServletRequest,
             HttpServletResponse httpServletResponse) {
-        String accessToken = authUseCase.checkRefreshToken(token, refreshToken);
+        String refreshToken = cookieUtil.getCookie(httpServletRequest, REFRESH_TOKEN).getValue();
+        String accessToken = authService.checkRefreshToken(token, refreshToken);
         httpServletResponse.setHeader("AUTHORIZATION", accessToken);
         return responseService.getSuccessResponse();
     }
