9116fb5d8a32ab9ac188d38886bd8722f08d1923
ParkJungYoon <tbr06057@naver.com>
2023-05-30 18:34:28 +0900

    feat: SecurityContextHolder에서 유저정보 추출해서 social id 반환하는 메서드 구현

---
+++ Module-API/src/main/java/depromeet/api/util/SecurityUtil.java
@@ -0,0 +1,14 @@
+package depromeet.api.util;
+
+import depromeet.domain.user.domain.Social;
+import depromeet.domain.user.domain.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtil {
+    public static String getCurrentUserSocialId() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User currentUser = (User) principal;
+        Social social = currentUser.getSocial();
+        return social.getId();
+    }
+}
