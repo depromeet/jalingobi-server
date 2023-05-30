5ee3b76e58c22900b785a160e19dd53d84db1408
ParkJungYoon <tbr06057@naver.com>
2023-05-30 15:16:19 +0900

    feat: record adaptor에 save 메서드 추가

--- Module-Domain/src/main/java/depromeet/domain/record/adaptor/RecordAdaptor.java
+++ Module-Domain/src/main/java/depromeet/domain/record/adaptor/RecordAdaptor.java
@@ -2,6 +2,7 @@ package depromeet.domain.record.adaptor;
 
 
 import depromeet.common.annotation.Adaptor;
+import depromeet.domain.record.domain.Record;
 import depromeet.domain.record.repository.RecordRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +10,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RecordAdaptor {
     private final RecordRepository recordRepository;
+
+    public Record save(Record record) {
+        return recordRepository.save(record);
+    }
 }
