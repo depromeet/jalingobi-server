03cb66a0918fc960069ecaaf9b50b64d9316d254
likelasttime <46569105+likelasttime@users.noreply.github.com>
2023-05-31 14:08:20 +0900

    [Feature] Refresh Token으로 Access Token을 재발급받는 reissue 추가 (#39)
    * feat: reissue API 추가 (#36)
    
    * test: reissue API 테스트 추가 (#36)
    
    * feat: refreshToken을 Cookie에서 가져오도록 변경 (#36)
    
    refreshToken은 Cookie에 보관할 것이라서 수정함
    
    * test: API 수정에 따른 테스트 코드 수정 (#36)

--- Module-API/src/main/java/depromeet/api/domain/auth/controller/AuthController.java
+++ Module-API/src/main/java/depromeet/api/domain/auth/controller/AuthController.java
@@ -2,13 +2,18 @@ package depromeet.api.domain.auth.controller;
 
 
 import depromeet.api.domain.auth.dto.request.AuthRequest;
+import depromeet.api.domain.auth.service.AuthService;
 import depromeet.api.domain.auth.usecase.KakaoAuthUseCase;
+import depromeet.api.util.CookieUtil;
 import depromeet.common.response.CommonResponse;
 import depromeet.common.response.ResponseService;
 import depromeet.common.temp.TokenInfo;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -17,10 +22,24 @@ public class AuthController {
 
     private final ResponseService responseService;
     private final KakaoAuthUseCase kakaoAuthUseCase;
+    private final AuthService authService;
+    private final CookieUtil cookieUtil;
+    private static final String REFRESH_TOKEN = "refreshToken";
 
     @PostMapping("/auth/kakao")
     public CommonResponse auth(@RequestBody AuthRequest reqAuth) {
         TokenInfo tokenInfo = kakaoAuthUseCase.execute(reqAuth);
         return responseService.getDataResponse(tokenInfo);
+    }
+
+    @PostMapping("/auth/refresh")
+    public CommonResponse refreshToken(
+            @RequestHeader("AUTHORIZATION") String token,
+            HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse) {
+        String refreshToken = cookieUtil.getCookie(httpServletRequest, REFRESH_TOKEN).getValue();
+        String accessToken = authService.checkRefreshToken(token, refreshToken);
+        httpServletResponse.setHeader("AUTHORIZATION", accessToken);
+        return responseService.getSuccessResponse();
     }
 }
---
+++ Module-API/src/test/java/depromeet/api/controller/AuthControllerTest.java
@@ -0,0 +1,72 @@
+package depromeet.api.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import depromeet.api.config.security.filter.JwtRequestFilter;
+import depromeet.api.domain.auth.controller.AuthController;
+import depromeet.api.domain.auth.service.AuthService;
+import depromeet.api.domain.auth.usecase.KakaoAuthUseCase;
+import depromeet.api.util.CookieUtil;
+import depromeet.common.response.ResponseService;
+import javax.servlet.http.Cookie;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(
+        controllers = AuthController.class,
+        excludeFilters = {
+            @ComponentScan.Filter(
+                    type = FilterType.ASSIGNABLE_TYPE,
+                    classes = {JwtRequestFilter.class})
+        })
+@AutoConfigureDataJpa
+@AutoConfigureMockMvc(addFilters = false)
+public class AuthControllerTest {
+
+    @Autowired MockMvc mockMvc;
+
+    @MockBean ResponseService responseService;
+
+    @MockBean KakaoAuthUseCase kakaoAuthUseCase;
+
+    @MockBean CookieUtil cookieUtil;
+
+    @MockBean AuthService authService;
+
+    @Test
+    @DisplayName("Refresh Token을 이용한 Access Token 재발급")
+    public void refreshTokenTest() throws Exception {
+        String token = "access-token";
+        String refreshToken = "refresh-token";
+        String newAccessToken = "new-access-token";
+        Cookie cookie = new Cookie("REFRESH_TOKEN", refreshToken);
+
+        MockHttpServletRequestBuilder requestBuilder =
+                MockMvcRequestBuilders.post("/auth/refresh")
+                        .header("AUTHORIZATION", token)
+                        .header("REFRESH-TOKEN", refreshToken)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        when(cookieUtil.getCookie(any(), anyString())).thenReturn(cookie);
+        when(authService.checkRefreshToken(anyString(), anyString())).thenReturn(newAccessToken);
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpectAll(status().isOk(), header().string("AUTHORIZATION", newAccessToken));
+    }
+}
