5bba338de32dddb980f5dae1900bf39f512d9cb7
ParkJungYoon <tbr06057@naver.com>
2023-05-31 14:05:23 +0900

    style: spotless 적용

--- Module-API/src/main/java/depromeet/api/domain/record/dto/request/CreateRecordRequest.java
+++ Module-API/src/main/java/depromeet/api/domain/record/dto/request/CreateRecordRequest.java
@@ -6,8 +6,8 @@ import javax.validation.constraints.Size;
 import lombok.Getter;
 
 /**
- *  @NotNull은 값이 null이 아니어야 하며, @NotBlank는 값이 null이 아니고 비어 있지 않아야 하며, 최소한 하나 이상의 비공백 문자를 포함
- *  기획파트에서 공백 문자도 허용하는지 묻고, 수정하기
+ * @NotNull은 값이 null이 아니어야 하며, @NotBlank는 값이 null이 아니고 비어 있지 않아야 하며, 최소한 하나 이상의 비공백 문자를 포함 기획파트에서 공백
+ * 문자도 허용하는지 묻고, 수정하기
  */
 @Getter
 public class CreateRecordRequest {
