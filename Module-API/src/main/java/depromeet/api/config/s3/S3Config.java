062710bb1897ab19f49ace84ac9b2e1db4e7d95c
ParkJungYoon <tbr06057@naver.com>
2023-06-02 07:38:07 +0900

    feat: S3 Config 작성

---
+++ Module-API/src/main/java/depromeet/api/config/s3/S3Config.java
@@ -0,0 +1,34 @@
+ package depromeet.api.config.s3;
+
+
+ import com.amazonaws.auth.AWSCredentials;
+ import com.amazonaws.auth.AWSStaticCredentialsProvider;
+ import com.amazonaws.auth.BasicAWSCredentials;
+ import com.amazonaws.regions.Regions;
+ import com.amazonaws.services.s3.AmazonS3;
+ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+ import org.springframework.beans.factory.annotation.Value;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+
+ @Configuration
+ public class S3Config {
+
+    @Value("${cloud.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.credentials.secret-key}")
+    private String secretKey;
+
+     @Value("${cloud.aws.region.static}")
+     private String region;
+
+    @Bean
+    public AmazonS3 getS3ClientBean() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(this.region)
+                .build();
+    }
+ }
