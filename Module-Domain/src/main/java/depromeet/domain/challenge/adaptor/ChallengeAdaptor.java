af1b08c2dcf072b0b70fa31f9466ab4ca3945008
likelasttime <a50070863@gmail.com>
2023-06-03 00:14:46 +0900

    feat: 레포지토리 계층 추가 (#53)

---
+++ Module-Domain/src/main/java/depromeet/domain/challenge/adaptor/ChallengeAdaptor.java
@@ -0,0 +1,18 @@
+package depromeet.domain.challenge.adaptor;
+
+
+import depromeet.common.annotation.Adaptor;
+import depromeet.domain.challenge.domain.Challenge;
+import depromeet.domain.challenge.repository.ChallengeRepository;
+import lombok.RequiredArgsConstructor;
+
+@Adaptor
+@RequiredArgsConstructor
+public class ChallengeAdaptor {
+
+    private final ChallengeRepository challengeRepository;
+
+    public Challenge save(Challenge challenge) {
+        return challengeRepository.save(challenge);
+    }
+}
