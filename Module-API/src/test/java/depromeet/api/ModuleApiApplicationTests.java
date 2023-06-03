9130d4fb3ee5facc4de76cf5c9a3a93be233caef
ParkJungYoon <tbr06057@naver.com>
2023-06-03 13:49:35 +0900

    refactor: 불필요한 코드 제거

--- Module-API/src/test/java/depromeet/api/ModuleApiApplicationTests.java
+++ Module-API/src/test/java/depromeet/api/ModuleApiApplicationTests.java
@@ -1,22 +1,9 @@
 package depromeet.api;
 
 
-import depromeet.api.util.JwtUtil;
-import depromeet.common.response.ResponseService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {ModuleApiApplication.class})
-@Import(HelloController.class)
 class ModuleApiApplicationTests {
-    @MockBean private JwtUtil jwtUtil;
-
-    @MockBean private ResponseService responseService;
-    @Autowired MockMvc mvc;
 
     @Test
     void contextLoads() {}
