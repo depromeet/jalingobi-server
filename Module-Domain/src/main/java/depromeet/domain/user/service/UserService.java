97db5c051b64f4f213c04db91d7ffd63c3c86207
imdh <eastlake520@gmail.com>
2023-06-03 16:02:40 +0900

    [Test] 소셜로그인 도메인 테스트 코드 작성 (#59)
    * test: UserAdaptor TC 작성
    
    * test: UserService TC 작성

--- Module-Domain/src/main/java/depromeet/domain/user/service/UserService.java
+++ Module-Domain/src/main/java/depromeet/domain/user/service/UserService.java
@@ -4,9 +4,8 @@ package depromeet.domain.user.service;
 import depromeet.common.annotation.DomainService;
 import depromeet.domain.user.domain.Platform;
 import depromeet.domain.user.domain.User;
-import depromeet.domain.user.exception.DuplicatedEmailException;
+import depromeet.domain.user.exception.AccountAlreadyExist;
 import depromeet.domain.user.repository.UserRepository;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -15,17 +14,14 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    public User login(Platform platform, User user) {
+        if (!platform.equals(user.getSocial().getPlatform())) throw AccountAlreadyExist.EXCEPTION;
+
+        return user;
+    }
+
     public User register(String nickname, String email, String socialId, Platform platform) {
         User newUser = User.registerUser(nickname, email, socialId, platform);
         return userRepository.save(newUser);
-    }
-
-    public User login(Platform platform, Optional<User> findUser) {
-        User user = findUser.get();
-
-        if (!platform.equals(user.getSocial().getPlatform()))
-            throw DuplicatedEmailException.EXCEPTION;
-
-        return user;
     }
 }
---
+++ Module-Domain/src/test/java/depromeet/domain/user/service/UserServiceTest.java
@@ -0,0 +1,74 @@
+package depromeet.domain.user.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import depromeet.domain.user.domain.Platform;
+import depromeet.domain.user.domain.User;
+import depromeet.domain.user.exception.AccountAlreadyExist;
+import depromeet.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    @InjectMocks private UserService userService;
+
+    @Mock private UserRepository userRepository;
+
+    @Test
+    public void register_성공() throws Exception {
+        // given
+        Platform platform = Platform.KAKAO;
+        String nickname = "tester";
+        String email = "test@test";
+        String socialId = "1234";
+        User user = User.createUser(nickname, email, socialId, platform);
+
+        given(userRepository.save(any())).willReturn(user);
+
+        // when
+        User registerUser = userService.register(nickname, email, socialId, platform);
+
+        // then
+        assertThat(registerUser).usingRecursiveComparison().isEqualTo(user);
+    }
+
+    @Test
+    public void login_성공() throws Exception {
+        // given
+        Platform platform = Platform.KAKAO;
+        String nickname = "tester";
+        String email = "test@test";
+        String socialId = "1234";
+        User user = User.createUser(nickname, email, socialId, platform);
+
+        // when then
+        User loginUser = userService.login(Platform.KAKAO, user);
+
+        // then
+        assertThat(loginUser).usingRecursiveComparison().isEqualTo(user);
+    }
+
+    @Test
+    public void login_실패_타플랫폼_가입된계정() throws Exception {
+        // given
+        Platform platform = Platform.KAKAO;
+        String nickname = "tester";
+        String email = "test@test";
+        String socialId = "1234";
+        User user = User.createUser(nickname, email, socialId, platform);
+
+        // when then
+        Assertions.assertThrows(
+                AccountAlreadyExist.class,
+                () -> {
+                    userService.login(Platform.APPLE, user);
+                });
+    }
+}
