aeda8226b3d13388c4ecf7357286b914def2ae93
ParkJungYoon <tbr06057@naver.com>
2023-06-04 23:44:22 +0900

    fix: createRecord 변경에 따라 test 코드 수정

--- Module-Domain/src/test/java/depromeet/domain/record/adaptor/RecordAdaptorTest.java
+++ Module-Domain/src/test/java/depromeet/domain/record/adaptor/RecordAdaptorTest.java
@@ -8,6 +8,10 @@ import static org.mockito.Mockito.when;
 import depromeet.domain.record.domain.Record;
 import depromeet.domain.record.domain.RecordEvaluation;
 import depromeet.domain.record.repository.RecordRepository;
+import depromeet.domain.user.domain.Platform;
+import depromeet.domain.user.domain.Profile;
+import depromeet.domain.user.domain.Social;
+import depromeet.domain.user.domain.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -33,10 +37,21 @@ class RecordAdaptorTest {
     public void RecordAdaptorTest() throws Exception {
         // given
         // 메서드가 record 객체를 반환하도록 mockito 설정
-        Record record = Record.createRecord(1L, 1L, 4000, "커피", "커피는 무죄", "", RecordEvaluation.ONE);
-        when(recordRepository.save(record)).thenReturn(record);
+        Platform platform = Platform.KAKAO;
+        String nickname = "tester";
+        String email = "test@test";
+        String socialId = "1234";
+        Profile profile = Profile.createProfile(nickname, email, null);
+        Social social = Social.createSocial(socialId, platform);
+        User user = User.builder().profile(profile).social(social).build();
+
+        Record record =
+                Record.createRecord(1L, user, 4000, "커피", "커피는 무죄", "", RecordEvaluation.ONE);
+
         // when
+        when(recordRepository.save(record)).thenReturn(record);
         Record savedRecord = recordAdaptor.save(record);
+
         // then
         // 예상한 결과와 실제 결과가 일치하는지 확인
         verify(recordRepository, times(1)).save(record);
