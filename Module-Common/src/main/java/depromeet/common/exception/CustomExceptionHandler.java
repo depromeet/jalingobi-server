e098067507cc5a2ec629f11f7834571871e06a6d
likelasttime <a50070863@gmail.com>
2023-06-04 17:04:42 +0900

    refactor: 정적 메서드로 변경 (#68)

--- Module-Common/src/main/java/depromeet/common/exception/CustomExceptionHandler.java
+++ Module-Common/src/main/java/depromeet/common/exception/CustomExceptionHandler.java
@@ -3,22 +3,18 @@ package depromeet.common.exception;
 
 import depromeet.common.response.CommonResponse;
 import depromeet.common.response.ResponseService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-@RequiredArgsConstructor
 public class CustomExceptionHandler {
-
-    private final ResponseService responseService;
 
     @ExceptionHandler(CustomException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     protected CommonResponse customException(CustomException customException) {
         CustomExceptionStatus status = customException.getCustomExceptionStatus();
-        return responseService.getExceptionResponse(status);
+        return ResponseService.getExceptionResponse(status);
     }
 }
