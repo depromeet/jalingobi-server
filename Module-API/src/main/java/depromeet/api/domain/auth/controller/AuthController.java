8db8fafe9d286481a05f1c1311d33d372d10fa13
imdh <eastlake520@gmail.com>
2023-06-05 15:32:31 +0900

    docs: 카카오 소셜로그인 API 문서 작성

--- Module-API/src/main/java/depromeet/api/domain/auth/controller/AuthController.java
+++ Module-API/src/main/java/depromeet/api/domain/auth/controller/AuthController.java
@@ -7,16 +7,26 @@ import depromeet.api.domain.auth.usecase.AuthUseCase;
 import depromeet.api.domain.auth.usecase.KakaoAuthUseCase;
 import depromeet.api.util.CookieUtil;
 import depromeet.common.response.CommonResponse;
+import depromeet.common.response.Response;
 import depromeet.common.response.ResponseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "인증", description = "인증 API")
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 @RestController
 public class AuthController {
 
@@ -24,8 +34,24 @@ public class AuthController {
     private final AuthUseCase authUseCase;
     private final CookieUtil cookieUtil;
 
-    @PostMapping("/auth/kakao")
-    public CommonResponse authKakao(
+    @Operation(
+            summary = "KAKAO 소셜로그인 API",
+            description = "사용자 인증 이후, access[body]/refresh[cookie] token을 발급합니다.")
+    @ApiResponses(
+            value = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "Set-Cookie에 RefreshToken={토큰값} 형태로 RefreshToken이 들어있습니다.",
+                        headers = {
+                            @Header(name = "Set-Cookie", description = "RefreshToken={value}")
+                        }),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "유효하지 않은 토큰으로 요청하거나, 다른 플랫폼에 해당 이메일로 가입된 계정이 있을 경우",
+                        content = {@Content(mediaType = "application/json")})
+            })
+    @PostMapping("/kakao")
+    public Response<KakaoAuthResponse> authKakao(
             @RequestBody @Valid KakaoAuthRequest reqAuth, HttpServletResponse response) {
 
         KakaoAuthResponse kakaoAuthResponse = kakaoAuthUseCase.execute(reqAuth);
@@ -33,7 +59,7 @@ public class AuthController {
         return ResponseService.getDataResponse(kakaoAuthResponse);
     }
 
-    @PostMapping("/auth/refresh")
+    @PostMapping("/refresh")
     public CommonResponse refreshToken(HttpServletRequest request) {
         String refreshToken = cookieUtil.getCookie(request, "RefreshToken").getValue();
         String accessToken = authUseCase.checkRefreshToken(refreshToken);
