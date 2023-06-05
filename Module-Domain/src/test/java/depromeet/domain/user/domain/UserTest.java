d6d70f323059d369f968c4cbe80504a2e2873ccf
imdh <eastlake520@gmail.com>
2023-06-05 15:09:02 +0900

    [Test] 카카오 소셜로그인 테스트 코드 추가 (#79)
    * test: authKakao tc 작성
    
    * test: registerUser tc 작성

---
+++ Module-Domain/src/test/java/depromeet/domain/user/domain/UserTest.java
@@ -0,0 +1,30 @@
+package depromeet.domain.user.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UserTest {
+
+    @Test
+    public void registerUser_성공() throws Exception {
+        // given
+        String nickname = "tester";
+        String email = "test@test";
+        String socialId = "1234";
+        Platform platform = Platform.KAKAO;
+
+        // when
+        User registerUser = User.registerUser(nickname, email, socialId, platform);
+
+        // then
+        assertThat(registerUser.getProfile().getName()).isEqualTo(nickname);
+        assertThat(registerUser.getProfile().getEmail()).isEqualTo(email);
+        assertThat(registerUser.getSocial().getId()).isEqualTo(socialId);
+        assertThat(registerUser.getSocial().getPlatform()).isEqualTo(platform);
+    }
+}
