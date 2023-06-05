5d7211f2e2f11851242d7a15438c5c5b99b67b52
imdh <eastlake520@gmail.com>
2023-06-06 08:43:10 +0900

    [Setting] Querydsl 설정 (#83)

--- Module-Domain/src/main/java/depromeet/domain/record/repository/RecordRepository.java
+++ Module-Domain/src/main/java/depromeet/domain/record/repository/RecordRepository.java
@@ -4,4 +4,4 @@ package depromeet.domain.record.repository;
 import depromeet.domain.record.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecordRepository extends JpaRepository<Record, Long> {}
+public interface RecordRepository extends JpaRepository<Record, Long>, RecordCustomRepository {}
