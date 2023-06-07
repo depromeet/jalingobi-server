577c50200548d81cc33141bf3d63aa03ca40f87e
likelasttime <a50070863@gmail.com>
2023-06-07 09:59:25 +0900

    refactor: CreateChallengeUseCase 수정 (#89)
    저장된 categoryId를 가져옴

--- Module-API/src/main/java/depromeet/api/domain/challenge/usecase/CreateChallengeUseCase.java
+++ Module-API/src/main/java/depromeet/api/domain/challenge/usecase/CreateChallengeUseCase.java
@@ -5,6 +5,8 @@ import depromeet.api.domain.challenge.dto.request.CreateChallengeRequest;
 import depromeet.api.domain.challenge.dto.response.CreateChallengeResponse;
 import depromeet.api.domain.challenge.mapper.ChallengeMapper;
 import depromeet.common.annotation.UseCase;
+import depromeet.domain.category.adaptor.CategoryAdaptor;
+import depromeet.domain.category.domain.Category;
 import depromeet.domain.challenge.adaptor.ChallengeAdaptor;
 import depromeet.domain.challenge.domain.Challenge;
 import depromeet.domain.user.adaptor.UserAdaptor;
@@ -19,13 +21,18 @@ public class CreateChallengeUseCase {
 
     private final ChallengeMapper challengeMapper;
     private final ChallengeAdaptor challengeAdaptor;
+    private final CategoryAdaptor categoryAdaptor;
     private final UserAdaptor userAdaptor;
 
     @Transactional
     public CreateChallengeResponse execute(CreateChallengeRequest request, String socialId) {
         User currentUser = userAdaptor.findUser(socialId);
-        Challenge challenge = challengeMapper.toEntity(request, socialId);
+
+        Category savedCategory = categoryAdaptor.findByName(request.getCategory().getName());
+        Challenge challenge = challengeMapper.toEntity(request, socialId, savedCategory.getId());
         challenge.addRules(request.getChallengeRule());
-        return challengeMapper.toCreateChallengeResponse(challengeAdaptor.save(challenge));
+
+        return challengeMapper.toCreateChallengeResponse(
+                challengeAdaptor.save(challenge), savedCategory.getName());
     }
 }
