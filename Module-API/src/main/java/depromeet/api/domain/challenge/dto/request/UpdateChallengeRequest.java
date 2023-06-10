482616583255ee480308007bdf94fe3a23a9f450
likelasttime <a50070863@gmail.com>
2023-06-10 18:33:26 +0900

    refactor: UpdateChallengeRequest 수정 (#58)

--- Module-API/src/main/java/depromeet/api/domain/challenge/dto/request/UpdateChallengeRequest.java
+++ Module-API/src/main/java/depromeet/api/domain/challenge/dto/request/UpdateChallengeRequest.java
@@ -3,11 +3,15 @@ package depromeet.api.domain.challenge.dto.request;
 
 import java.time.LocalDate;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateChallengeRequest {
 
     private Long ChallengeId;
