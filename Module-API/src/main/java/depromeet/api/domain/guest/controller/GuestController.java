2b80134848d8b7ee52c0ec05d76ad4bc3736df60
imdh <eastlake520@gmail.com>
2023-06-10 23:04:35 +0900

    docs: 게스트 모드 API 문서 작성

--- Module-API/src/main/java/depromeet/api/domain/guest/controller/GuestController.java
+++ Module-API/src/main/java/depromeet/api/domain/guest/controller/GuestController.java
@@ -5,6 +5,10 @@ import depromeet.api.domain.guest.dto.ExperienceGuestModeResponse;
 import depromeet.api.domain.guest.usecase.ExperienceGuestModeUseCase;
 import depromeet.common.response.Response;
 import depromeet.common.response.ResponseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,15 @@ public class GuestController {
 
     private final ExperienceGuestModeUseCase experienceGuestModeUseCase;
 
+    @Operation(summary = "게스트 모드 API", description = "게스트 모드로 진입하기 위한 access token을 발급합니다.")
+    @ApiResponses(
+            value = {
+                @ApiResponse(responseCode = "200"),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "잘못된 요청값을 전달한 경우",
+                        content = @Content())
+            })
     @GetMapping("/guest")
     public Response<ExperienceGuestModeResponse> experienceGuestMode() {
 
--- Module-API/src/main/java/depromeet/api/domain/guest/dto/ExperienceGuestModeResponse.java
+++ Module-API/src/main/java/depromeet/api/domain/guest/dto/ExperienceGuestModeResponse.java
@@ -1,12 +1,15 @@
 package depromeet.api.domain.guest.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 public class ExperienceGuestModeResponse {
+
+    @Schema(example = "게스트 모드용 accessToken")
     private String accessToken;
 
     public static ExperienceGuestModeResponse of(String guestToken) {
