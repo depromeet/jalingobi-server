a28dfe20b3ccdd164ba93e2473158a1fe61374e7
likelasttime <a50070863@gmail.com>
2023-06-13 10:40:32 +0900

    feat: 이모지 도메인 추가 (#134)

--- Module-Domain/src/main/java/depromeet/domain/challenge/domain/UserChallenge.java
+++ Module-Domain/src/main/java/depromeet/domain/challenge/domain/UserChallenge.java
@@ -1,7 +1,10 @@
 package depromeet.domain.challenge.domain;
 
 
+import depromeet.domain.emoji.domain.Emoji;
+import depromeet.domain.record.domain.Record;
 import depromeet.domain.user.domain.User;
+import depromeet.domain.userchallenge.domain.Emojis;
 import javax.persistence.*;
 import lombok.*;
 
@@ -24,8 +27,19 @@ public class UserChallenge {
     @JoinColumn(name = "challenge_id", nullable = false)
     private Challenge challenge;
 
+    @Embedded private Emojis emojis;
+
     private String nickname;
 
     @Column(name = "current_charge")
     private int currentCharge;
+
+    public void reactEmoji(Record record, String type) {
+        Emoji emoji = Emoji.createEmoji(this, record, type);
+        emojis.add(emoji);
+    }
+
+    public int getEmojiCounts() {
+        return emojis.getCounts();
+    }
 }
