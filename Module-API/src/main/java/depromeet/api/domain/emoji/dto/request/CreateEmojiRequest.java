bc058bc2934c978b481fcd5dce07a7a9b8891f24
likelasttime <a50070863@gmail.com>
2023-06-13 12:05:34 +0900

    feat: CreateEmojiRequest 수정 (#134)

--- Module-API/src/main/java/depromeet/api/domain/emoji/dto/request/CreateEmojiRequest.java
+++ Module-API/src/main/java/depromeet/api/domain/emoji/dto/request/CreateEmojiRequest.java
@@ -1,12 +1,18 @@
 package depromeet.api.domain.emoji.dto.request;
 
 
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateEmojiRequest {
 
+    @NotBlank(message = "이모지 타입을 입력하세요.")
     private String type;
 }
