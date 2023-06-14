447b2e5bc71efc601c0898bf108f9aeb13ca09a3
ParkJungYoon <tbr06057@naver.com>
2023-06-15 02:00:14 +0900

    feat: record img가 null이 아닌 경우에 s3에 삭제하는 로직 추가

--- Module-API/src/main/java/depromeet/api/domain/record/usecase/UpdateRecordUseCase.java
+++ Module-API/src/main/java/depromeet/api/domain/record/usecase/UpdateRecordUseCase.java
@@ -1,25 +1,39 @@
 package depromeet.api.domain.record.usecase;
 
 
+import depromeet.api.config.s3.S3UploadPresignedUrlService;
 import depromeet.api.domain.record.dto.request.CreateRecordRequest;
 import depromeet.api.domain.record.validator.RecordValidator;
 import depromeet.common.annotation.UseCase;
 import depromeet.domain.record.adaptor.RecordAdaptor;
 import depromeet.domain.record.domain.Record;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
 
 @UseCase
 @Transactional
 @RequiredArgsConstructor
 public class UpdateRecordUseCase {
+    @Value("${image.prefix}")
+    private String prefix;
+
     private final RecordAdaptor recordAdaptor;
     private final RecordValidator recordValidator;
+    private final S3UploadPresignedUrlService uploadPresignedUrlService;
 
     public void execute(Long recordId, String socialId, CreateRecordRequest updateRecordRequest) {
         Record record = recordAdaptor.findRecord(recordId);
 
         recordValidator.validateCorrectUserRecord(record, socialId);
+
+        Optional<String> currentImgUrl = Optional.ofNullable(record.getImgUrl());
+        if (currentImgUrl.isPresent()) {
+            String imgUrl = currentImgUrl.get();
+            String key = getKey(imgUrl);
+            uploadPresignedUrlService.deleteImage(key);
+        }
 
         record.updateRecord(
                 updateRecordRequest.getPrice(),
@@ -27,5 +41,10 @@ public class UpdateRecordUseCase {
                 updateRecordRequest.getContent(),
                 updateRecordRequest.getImgUrl(),
                 updateRecordRequest.getEvaluation());
+    }
+
+    private String getKey(String imgUrl) {
+        String[] splitUrl = imgUrl.split(prefix);
+        return splitUrl[1];
     }
 }
