1abc932914566dc0aacca6dcd87aaed9962cae1b
likelasttime <a50070863@gmail.com>
2023-06-15 14:35:22 +0900

    feat: Comment 수정 repository 계층 추가 (#166)

--- Module-Domain/src/main/java/depromeet/domain/comment/adaptor/CommentAdaptor.java
+++ Module-Domain/src/main/java/depromeet/domain/comment/adaptor/CommentAdaptor.java
@@ -3,6 +3,7 @@ package depromeet.domain.comment.adaptor;
 
 import depromeet.common.annotation.Adaptor;
 import depromeet.domain.comment.domain.Comment;
+import depromeet.domain.comment.exception.CommentNotFoundException;
 import depromeet.domain.comment.repository.CommentRepository;
 import depromeet.domain.record.domain.Record;
 import depromeet.domain.record.exception.RecordNotFoundException;
@@ -27,5 +28,15 @@ public class CommentAdaptor {
         Comment comment = Comment.create(record.getUserChallenge(), record, content);
 
         return commentRepository.save(comment);
+    }
+
+    @Transactional
+    public void updateComment(String socialId, String content, Long commentId) {
+        Comment savedComment =
+                commentRepository
+                        .findById(commentId)
+                        .orElseThrow(() -> CommentNotFoundException.EXCEPTION);
+        savedComment.isCommenter(socialId);
+        savedComment.update(content);
     }
 }
