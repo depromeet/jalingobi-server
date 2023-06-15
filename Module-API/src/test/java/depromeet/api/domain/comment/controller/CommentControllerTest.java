396e103fccc403a91e1a62ac3ddf270303d87602
likelasttime <a50070863@gmail.com>
2023-06-15 14:39:58 +0900

    test: Comment 수정 API 테스트 추가 (#166)

--- Module-API/src/test/java/depromeet/api/domain/comment/controller/CommentControllerTest.java
+++ Module-API/src/test/java/depromeet/api/domain/comment/controller/CommentControllerTest.java
@@ -2,8 +2,10 @@ package depromeet.api.domain.comment.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,11 +13,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import depromeet.api.config.security.filter.JwtRequestFilter;
 import depromeet.api.domain.comment.dto.request.CreateCommentRequest;
+import depromeet.api.domain.comment.dto.request.UpdateCommentRequest;
 import depromeet.api.domain.comment.dto.response.CreateCommentResponse;
 import depromeet.api.domain.comment.usecase.CreateCommentUseCase;
+import depromeet.api.domain.comment.usecase.UpdateCommentUseCase;
+import depromeet.api.util.AuthenticationUtil;
 import java.time.LocalDateTime;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -42,6 +50,22 @@ public class CommentControllerTest {
     @Autowired ObjectMapper objectMapper;
 
     @MockBean CreateCommentUseCase createCommentUseCase;
+
+    @MockBean UpdateCommentUseCase updateCommentUseCase;
+
+    private static MockedStatic<AuthenticationUtil> authenticationUtil;
+
+    @BeforeAll
+    public static void beforeALl() {
+        authenticationUtil = mockStatic(AuthenticationUtil.class);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        authenticationUtil.close();
+    }
+
+    String socialId = "socialId";
 
     @Test
     @DisplayName("댓글 생성")
@@ -73,5 +97,24 @@ public class CommentControllerTest {
                         jsonPath("$.result.content").value(response.getContent()));
 
         verify(createCommentUseCase, times(1)).execute(anyLong(), any());
+    }
+
+    @Test
+    @DisplayName("댓글 수정")
+    public void updateCommentTest() throws Exception {
+        UpdateCommentRequest request = UpdateCommentRequest.builder().content("반가워요!").build();
+
+        when(AuthenticationUtil.getCurrentUserSocialId()).thenReturn(socialId);
+        willDoNothing().given(updateCommentUseCase).execute(any(), anyString(), anyLong());
+
+        mockMvc.perform(
+                        put("/records/{recordId}/comments/{commentId}", 1L, 1L)
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .characterEncoding("UTF-8"))
+                .andDo(print())
+                .andExpectAll(status().isOk());
+
+        verify(updateCommentUseCase, times(1)).execute(any(), anyString(), anyLong());
     }
 }
