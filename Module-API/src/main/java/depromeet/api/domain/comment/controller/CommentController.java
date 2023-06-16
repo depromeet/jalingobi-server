5d467c65ad5fa8dc5e517444ad79b6dad077f18a
likelasttime <a50070863@gmail.com>
2023-06-16 16:43:32 +0900

    docs: 댓글 API 문서 작성 (#177)

--- Module-API/src/main/java/depromeet/api/domain/comment/controller/CommentController.java
+++ Module-API/src/main/java/depromeet/api/domain/comment/controller/CommentController.java
@@ -11,6 +11,7 @@ import depromeet.api.domain.comment.usecase.UpdateCommentUseCase;
 import depromeet.common.response.CommonResponse;
 import depromeet.common.response.Response;
 import depromeet.common.response.ResponseService;
+import io.swagger.v3.oas.annotations.Operation;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -24,12 +25,14 @@ public class CommentController {
     private final UpdateCommentUseCase updateCommentUseCase;
     private final DeleteCommentUseCase deleteCommentUsecase;
 
+    @Operation(summary = "댓글 생성 API", description = "댓글을 생성합니다.")
     @PostMapping("/{recordId}/comment")
     public Response<CreateCommentResponse> createComment(
             @PathVariable long recordId, @Valid @RequestBody CreateCommentRequest request) {
         return ResponseService.getDataResponse(createCommentUseCase.execute(recordId, request));
     }
 
+    @Operation(summary = "댓글 수정 API", description = "댓글을 수정합니다.")
     @PutMapping("/{recordId}/comment/{commentId}")
     public CommonResponse updateComment(
             @PathVariable long recordId,
@@ -39,6 +42,7 @@ public class CommentController {
         return ResponseService.getSuccessResponse();
     }
 
+    @Operation(summary = "댓글 삭제 API", description = "댓글을 삭제합니다.")
     @DeleteMapping("/{recordId}/comment/{commentId}")
     public CommonResponse deleteComment(@PathVariable long recordId, @PathVariable long commentId) {
         deleteCommentUsecase.execute(getCurrentUserSocialId(), commentId);
--- Module-API/src/main/java/depromeet/api/domain/comment/dto/request/UpdateCommentRequest.java
+++ Module-API/src/main/java/depromeet/api/domain/comment/dto/request/UpdateCommentRequest.java
@@ -1,6 +1,7 @@
 package depromeet.api.domain.comment.dto.request;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import lombok.Getter;
 @Builder
 public class UpdateCommentRequest {
 
+    @Schema(description = "댓글 내용", example = "안녕")
     @NotBlank(message = "댓글을 입력하세요.")
     private String content;
 
