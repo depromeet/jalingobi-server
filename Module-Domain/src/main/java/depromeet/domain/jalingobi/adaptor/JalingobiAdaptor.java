637faf920d9ef6019f027639513f8e7a4c2237f2
ParkJungYoon <tbr06057@naver.com>
2023-06-16 16:51:58 +0900

    feat: 자린고비 정보 전체 조회 메서드 구현

--- Module-Domain/src/main/java/depromeet/domain/jalingobi/adaptor/JalingobiAdaptor.java
+++ Module-Domain/src/main/java/depromeet/domain/jalingobi/adaptor/JalingobiAdaptor.java
@@ -6,12 +6,21 @@ import depromeet.domain.jalingobi.domain.Jalingobi;
 import depromeet.domain.jalingobi.domain.Level;
 import depromeet.domain.jalingobi.exception.JalingobiNotFoundException;
 import depromeet.domain.jalingobi.repository.JalingobiRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 
 @Adaptor
 @RequiredArgsConstructor
 public class JalingobiAdaptor {
     private final JalingobiRepository jalingobiRepository;
+
+    public List<Jalingobi> findAll() {
+        List<Jalingobi> jalingobiList = jalingobiRepository.findAll();
+        if (jalingobiList.isEmpty()) {
+            throw JalingobiNotFoundException.EXCEPTION;
+        }
+        return jalingobiList;
+    }
 
     public Jalingobi findJalingobi(Level level) {
         return jalingobiRepository
