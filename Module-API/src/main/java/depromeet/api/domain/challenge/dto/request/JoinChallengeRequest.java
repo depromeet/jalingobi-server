6984b6bf3cd0014fc46857ba5fbcc9b8fbbe2eec
likelasttime <a50070863@gmail.com>
2023-06-18 12:29:30 +0900

    feat: 챌린지 생성 요청 DTO에 필드 추가 (#202)

--- Module-API/src/main/java/depromeet/api/domain/challenge/dto/request/JoinChallengeRequest.java
+++ Module-API/src/main/java/depromeet/api/domain/challenge/dto/request/JoinChallengeRequest.java
@@ -1,6 +1,7 @@
 package depromeet.api.domain.challenge.dto.request;
 
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,9 +14,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class JoinChallengeRequest {
 
-    @NotNull(message = "챌린지 동안 사용할 닉네임 입력하세요.")
+    @NotBlank(message = "챌린지 동안 사용할 닉네임 입력하세요.")
     private String nickname;
 
     @NotNull(message = "참여하는 챌린지의 목표 금액을 입력하세요.")
     private Integer currentCharge;
+
+    @NotBlank(message = "프로필 이미지 경로를 입력하세요.")
+    private String imgUrl;
 }
