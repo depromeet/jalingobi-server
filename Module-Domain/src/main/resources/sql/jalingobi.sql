9817078367dc8c553af667bfb98cf6f060308475
imdh <eastlake520@gmail.com>
2023-06-18 10:26:54 +0900

    [Setting] sql 파일 추가 (#208)
    * feat: category 테이블의 pk 컬럼 이름 설정
    
    * feat: 데이터 중복 제거를 위한 pk값 설정 및 IGNORE 설정
    
    * setting: jalingobi, small_talk, category 데이터 sql 파일 설정

---
+++ Module-Domain/src/main/resources/sql/jalingobi.sql
@@ -0,0 +1,20 @@
+INSERT IGNORE INTO JALINGOBI (jalingobi_id, level, name, img_url, acquisition_condition)
+VALUES (1, "LEVEL_0", "보릿고개를 나는", "image.png", "0점");
+
+INSERT IGNORE INTO JALINGOBI (jalingobi_id, level, name, img_url, acquisition_condition)
+VALUES (2, "LEVEL_1", "그럭 저럭 살고 있는", "image.png", "1점");
+
+INSERT IGNORE INTO JALINGOBI (jalingobi_id, level, name, img_url, acquisition_condition)
+VALUES (3, "LEVEL_2", "고비 없이 행복한", "image.png", "2점");
+
+INSERT IGNORE INTO JALINGOBI (jalingobi_id, level, name, img_url, acquisition_condition)
+VALUES (4, "LEVEL_3", "매일이 고기반찬인", "image.png", "3점");
+
+INSERT IGNORE INTO JALINGOBI (jalingobi_id, level, name, img_url, acquisition_condition)
+VALUES (5, "LEVEL_4", "곳간이 넘쳐나는", "image.png", "4점");
+
+INSERT IGNORE INTO JALINGOBI (jalingobi_id, level, name, img_url, acquisition_condition)
+VALUES (6, "LEVEL_5", "임금처럼 대접받는", "image.png", "5점");
+
+INSERT IGNORE INTO JALINGOBI (jalingobi_id, level, name, img_url, acquisition_condition)
+VALUES (7, "LEVEL_6", "우주 제일 부자인", "image.png", "6점");
