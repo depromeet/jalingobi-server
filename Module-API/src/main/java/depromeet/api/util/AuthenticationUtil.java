15b4430d658b4bbf89129f0a2dc9475c60600819
ParkJungYoon <tbr06057@naver.com>
2023-06-18 17:56:42 +0900

    chore: @Util 어노테이션 추가

--- Module-API/src/main/java/depromeet/api/util/AuthenticationUtil.java
+++ Module-API/src/main/java/depromeet/api/util/AuthenticationUtil.java
@@ -2,8 +2,10 @@ package depromeet.api.util;
 
 
 import depromeet.api.config.security.CustomUserDetails;
+import depromeet.common.annotation.Util;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+@Util
 public class AuthenticationUtil {
     public static String getCurrentUserSocialId() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
