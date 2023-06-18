630901d6cfa4e7ece5fe9e25878456ff5d71e575
ParkJungYoon <tbr06057@naver.com>
2023-06-19 00:17:49 +0900

    feat: 이미 탈퇴한 유저 예외 정의

---
+++ Module-Domain/src/main/java/depromeet/domain/user/exception/AlreadyWithdrawalUserException.java
@@ -0,0 +1,13 @@
+package depromeet.domain.user.exception;
+
+import depromeet.common.exception.CustomException;
+import depromeet.common.exception.CustomExceptionStatus;
+
+public class AlreadyWithdrawalUserException extends CustomException {
+
+    public static final CustomException EXCEPTION = new AlreadyWithdrawalUserException();
+
+    private AlreadyWithdrawalUserException() {
+        super(CustomExceptionStatus.ALREADY_WITHDRAWAL_USER);
+    }
+}
