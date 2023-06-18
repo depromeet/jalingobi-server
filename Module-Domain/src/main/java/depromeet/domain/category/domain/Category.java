1e8ff2e57304325e03ca5e0db7ad9a0b46fd8b74
imdh <eastlake520@gmail.com>
2023-06-18 09:49:39 +0900

    feat: category 테이블의 pk 컬럼 이름 설정

--- Module-Domain/src/main/java/depromeet/domain/category/domain/Category.java
+++ Module-Domain/src/main/java/depromeet/domain/category/domain/Category.java
@@ -14,6 +14,7 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
     private Long id;
 
     @Column private String name;
