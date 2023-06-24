077675e8d295a06deb28521599f29deca7337301
likelasttime <a50070863@gmail.com>
2023-06-24 22:00:34 +0900

    docs: DTO에 API 설명 추가 (#219)

--- Module-API/src/main/java/depromeet/api/domain/challenge/dto/response/HeadCountResponse.java
+++ Module-API/src/main/java/depromeet/api/domain/challenge/dto/response/HeadCountResponse.java
@@ -1,6 +1,7 @@
 package depromeet.api.domain.challenge.dto.response;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class HeadCountResponse {
 
+    @Schema(description = "챌린지 최대 수용 인원", example = "50")
     private int availableCount;
 
+    @Schema(description = "현재 챌린지 참가 인원", example = "30")
     private int participants;
 }
