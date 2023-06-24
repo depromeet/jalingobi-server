dea9cc327fe5821f0ebc73ead36eb8b941ee663f
likelasttime <a50070863@gmail.com>
2023-06-24 10:20:24 +0900

    refactor: 이모지 API 반환값 수정 (#253)

--- Module-API/src/main/java/depromeet/api/domain/emoji/controller/EmojiController.java
+++ Module-API/src/main/java/depromeet/api/domain/emoji/controller/EmojiController.java
@@ -4,11 +4,9 @@ import static depromeet.api.util.AuthenticationUtil.getCurrentUserSocialId;
 
 import depromeet.api.domain.emoji.dto.request.CreateEmojiRequest;
 import depromeet.api.domain.emoji.dto.request.DeleteEmojiRequest;
-import depromeet.api.domain.emoji.dto.response.CreateEmojiResponse;
-import depromeet.api.domain.emoji.dto.response.DeleteEmojiResponse;
 import depromeet.api.domain.emoji.usecase.CreateEmojiUseCase;
 import depromeet.api.domain.emoji.usecase.DeleteEmojiUseCase;
-import depromeet.common.response.Response;
+import depromeet.common.response.CommonResponse;
 import depromeet.common.response.ResponseService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
@@ -25,17 +23,17 @@ public class EmojiController {
     private final DeleteEmojiUseCase deleteEmojiUseCase;
 
     @PutMapping("/{recordId}/emoji")
-    public Response<CreateEmojiResponse> createRecordEmoji(
+    public CommonResponse createRecordEmoji(
             @PathVariable long recordId,
             @RequestBody @Valid CreateEmojiRequest createEmojiRequest) {
-        return ResponseService.getDataResponse(
-                createEmojiUseCase.execute(getCurrentUserSocialId(), recordId, createEmojiRequest));
+        createEmojiUseCase.execute(getCurrentUserSocialId(), recordId, createEmojiRequest);
+        return ResponseService.getSuccessResponse();
     }
 
     @DeleteMapping("/{recordId}/emoji")
-    public Response<DeleteEmojiResponse> deleteRecordEmoji(
+    public CommonResponse deleteRecordEmoji(
             @PathVariable long recordId, @RequestBody @Valid DeleteEmojiRequest request) {
-        return ResponseService.getDataResponse(
-                deleteEmojiUseCase.execute(getCurrentUserSocialId(), recordId, request.getType()));
+        deleteEmojiUseCase.execute(getCurrentUserSocialId(), recordId, request.getType());
+        return ResponseService.getSuccessResponse();
     }
 }
