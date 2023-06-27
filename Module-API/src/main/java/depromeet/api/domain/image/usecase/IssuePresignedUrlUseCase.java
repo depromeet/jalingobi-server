e842348c6aa873c9877d37e37a9d22dd15871550
ParkJungYoon <tbr06057@naver.com>
2023-06-27 17:58:28 +0900

    refactor: 존재하는 유저인지 확인하는 기능 구현

--- Module-API/src/main/java/depromeet/api/domain/image/usecase/IssuePresignedUrlUseCase.java
+++ Module-API/src/main/java/depromeet/api/domain/image/usecase/IssuePresignedUrlUseCase.java
@@ -5,15 +5,20 @@ import depromeet.api.config.s3.S3UploadPresignedUrlService;
 import depromeet.api.domain.image.dto.request.IssuePresignedUrlRequest;
 import depromeet.api.domain.image.dto.response.IssuePresignedUrlResponse;
 import depromeet.common.annotation.UseCase;
+import depromeet.domain.user.adaptor.UserAdaptor;
 import lombok.RequiredArgsConstructor;
 
 @UseCase
 @RequiredArgsConstructor
 public class IssuePresignedUrlUseCase {
+
+    private final UserAdaptor userAdaptor;
     private final S3UploadPresignedUrlService uploadPresignedUrlService;
 
     public IssuePresignedUrlResponse execute(
             String socialId, IssuePresignedUrlRequest issuePresignedUrlRequest) {
+
+        userAdaptor.doesUserExist(socialId);
 
         return IssuePresignedUrlResponse.from(
                 uploadPresignedUrlService.execute(
