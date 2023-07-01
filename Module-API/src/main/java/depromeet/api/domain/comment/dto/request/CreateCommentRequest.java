99642b8bf60ce8fc67c0abdcf868f384c70a02f5
likelasttime <a50070863@gmail.com>
2023-07-01 10:45:03 +0900

    feat: 댓글 길이 제한 추가 (#309)

--- Module-API/src/main/java/depromeet/api/domain/comment/dto/request/CreateCommentRequest.java
+++ Module-API/src/main/java/depromeet/api/domain/comment/dto/request/CreateCommentRequest.java
@@ -3,6 +3,7 @@ package depromeet.api.domain.comment.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,6 +13,7 @@ public class CreateCommentRequest {
 
     @Schema(description = "댓글 내용", example = "안녕")
     @NotBlank(message = "댓글을 입력하세요.")
+    @Size(min = 1, max = 2000, message = "내용은 2,000자 이하입니다.")
     private String content;
 
     private CreateCommentRequest() {}
