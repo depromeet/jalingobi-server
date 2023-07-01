f9ea96d250a5e27acdc69968c78204c9b3ff78a1
likelasttime <a50070863@gmail.com>
2023-07-01 17:27:51 +0900

    feat: DTO 및 Mapper 추가 (#321)

--- Module-API/src/main/java/depromeet/api/domain/challenge/dto/request/JoinChallengeRequest.java
+++ Module-API/src/main/java/depromeet/api/domain/challenge/dto/request/JoinChallengeRequest.java
@@ -2,6 +2,8 @@ package depromeet.api.domain.challenge.dto.request;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,9 +15,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class JoinChallengeRequest {
 
+    @NotBlank(message = "닉네임을 입력하세요.")
+    @Size(min = 1, max = 20, message = "닉네임은 20자 이하입니다.")
     @Schema(description = "챌린지에서 사용할 닉네임", example = "눈누난나")
     private String nickname;
 
+    @NotBlank(message = "프로필 이미지 경로를 입력하세요.")
     @Schema(description = "프로필 이미지", example = "/test.jpg")
     private String imgUrl;
 
