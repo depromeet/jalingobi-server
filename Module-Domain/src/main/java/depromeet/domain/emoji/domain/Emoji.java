0f1a0e060c768b4f6f842dcda5ad86a7f7fd5757
imdh <eastlake520@gmail.com>
2023-07-19 00:48:16 +0900

    bug: selected 값 정상적으로 보내주도록 수정
    selected 에 toString에 해당하는 값 넣어주고 있었음. 해당 부분 EmojiType이 제대로 들어가도록 수정

--- Module-Domain/src/main/java/depromeet/domain/emoji/domain/Emoji.java
+++ Module-Domain/src/main/java/depromeet/domain/emoji/domain/Emoji.java
@@ -38,6 +38,10 @@ public class Emoji {
                 .build();
     }
 
+    public String getType() {
+        return type.getValue();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
