def7104cb7892c712548bec8e0011c6aa9be5d3e
imdh <eastlake520@gmail.com>
2023-07-20 22:52:37 +0900

    [Feature] 챌린지 피드 기록에 challengeId 추가 (#403)

--- Module-API/src/main/java/depromeet/api/domain/feed/dto/ChallengeFeed.java
+++ Module-API/src/main/java/depromeet/api/domain/feed/dto/ChallengeFeed.java
@@ -20,6 +20,8 @@ public class ChallengeFeed {
 
     private FeedRecordInfo recordInfo;
 
+    private ChallengeInfo challengeInfo;
+
     private EmojiInfo emojiInfo;
 
     public static ChallengeFeed createChallengeFeed(Record record, Long userChallengeId) {
@@ -27,12 +29,14 @@ public class ChallengeFeed {
 
         FeedUserInfo userInfo = new FeedUserInfo(record.getUserChallenge());
         FeedRecordInfo recordInfo = new FeedRecordInfo(record);
+        ChallengeInfo challengeInfo = new ChallengeInfo(record.getChallenge().getId());
         EmojiInfo emojiInfo = EmojiInfo.createChallengeRoomEmojiInfo(record, userChallengeId);
 
         return ChallengeFeed.builder()
                 .isMine(isMine)
                 .userInfo(userInfo)
                 .recordInfo(recordInfo)
+                .challengeInfo(challengeInfo)
                 .emojiInfo(emojiInfo)
                 .build();
     }
@@ -54,6 +58,17 @@ public class ChallengeFeed {
             this.imgUrl = userChallenge.getImgUrl();
             this.nickname = userChallenge.getNickname();
             this.currentCharge = userChallenge.getCurrentCharge();
+        }
+    }
+
+    @Data
+    public static class ChallengeInfo {
+
+        @Schema(description = "챌린지 ID", example = "5")
+        private Long id;
+
+        public ChallengeInfo(Long id) {
+            this.id = id;
         }
     }
 }
