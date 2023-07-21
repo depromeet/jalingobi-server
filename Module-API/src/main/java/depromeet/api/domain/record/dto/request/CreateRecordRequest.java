a5cdaf06b2f8c5dda05a27a39a30c1d73ba8ef93
ParkJungYoon <tbr06057@naver.com>
2023-07-21 23:32:53 +0900

    refactor: 지출 평가 선택 값으로 수정

--- Module-API/src/main/java/depromeet/api/domain/record/dto/request/CreateRecordRequest.java
+++ Module-API/src/main/java/depromeet/api/domain/record/dto/request/CreateRecordRequest.java
@@ -38,7 +38,5 @@ public class CreateRecordRequest {
     private String imgUrl;
 
     @Schema(description = "지출 평가, WELLDONE | REGRETFUL | CRAZY 중에서 선택 가능합니다.")
-    @NotNull(message = "지출 평가를 입력해주세요.")
-    @ValidEnum(enumClass = Evaluation.class, message = "유효하지 않은 Evaluation 파라미터입니다.")
     private Evaluation evaluation;
 }
